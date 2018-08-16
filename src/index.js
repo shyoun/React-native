5325a4e6eaee24eeeb1cc7b1d365c079ab08f527
shyoun <27shyoun@gmail.com>
2018-08-16 17:55:49 +0900

    todo app.
    completed.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,75 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+let id = 0;
+
+const Todo = props => (
+  <li>
+    <input
+      type="checkbox"
+      checked={props.todo.checked}
+      onChange={props.onToggle}
+    />
+    <button onClick={props.onDelete}>delete</button>
+    <span>{props.todo.text}</span>
+  </li>
+);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  addTodo() {
+    const text = prompt("TODO text please!");
+    this.setState({
+      todos: [...this.state.todos, { text: text, id: id++, checked: false }]
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo;
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked
+        };
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Todo count: {this.state.todos.length}</div>
+        <div>
+          Unchecked todo count:{" "}
+          {this.state.todos.filter(todo => !todo.checked).length}
+        </div>
+        <button onClick={() => this.addTodo()}>Add TODO</button>
+        <ul>
+          {this.state.todos.map(todo => (
+            <Todo
+              onDelete={() => this.removeTodo(todo.id)}
+              onToggle={() => this.toggleTodo(todo.id)}
+              todo={todo}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
